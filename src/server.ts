a7b058e3d26375a824979802b0053eeed720a4d5
rashedul <rashedul@outlook.com>
2024-10-03 23:35:10 +0100

    Routes and middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// custom middleware
+app.use((req, res, next) => {
+    req.mysecret = ' lul';
+    next();
+})
 
 app.get('/', (req, res) => {
     console.log('GET from express');
     res.status(200);
     res.json({ message: 'Hello from express' });
 });
+
+app.use('/api', router);
 
 export default app;
